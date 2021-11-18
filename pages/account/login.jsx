c89c66c5cf5de34abaf9c40c89ed010d2eca4cc7
admin <karthickdurai587@gmail.com>
2021-11-18 16:56:07 +0530

    sfgd

--- pages/account/login.jsx
+++ pages/account/login.jsx
@@ -13,14 +13,14 @@ export default Login;
 function Login() {
     const router = useRouter();
 
-    // form validation rules 
+   
     const validationSchema = Yup.object().shape({
         username: Yup.string().required('Username is required'),
         password: Yup.string().required('Password is required')
     });
     const formOptions = { resolver: yupResolver(validationSchema) };
 
-    // get functions to build form with useForm() hook
+   
     const { register, handleSubmit, formState } = useForm(formOptions);
     const { errors } = formState;
 
@@ -42,30 +42,30 @@ function Login() {
       
             
        
-        <div class="form">
-  <div class="form-toggle"></div>
-  <div class="form-panel one">
-    <div class="form-header">
+        <div className="form">
+  <div className="form-toggle"></div>
+  <div className="form-panel one">
+    <div className="form-header">
       <h1>user Login</h1>
     </div>
-    <div class="form-content">
+    <div className="form-content">
       <form onSubmit={handleSubmit(onSubmit)}> 
-        <div class="form-group">
-          <label for="username">Username</label>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
           <input name="username" type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
                             <div className="invalid-feedback">{errors.username?.message}</div>
         </div>
-        <div class="form-group">
-          <label for="password">Password</label>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
           <input name="password" type="password" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
                             <div className="invalid-feedback">{errors.password?.message}</div>
         </div>
-        <div class="form-group">
-          <label class="form-remember">
+        <div className="form-group">
+          <label className="form-remember">
             <input type="checkbox"/>Remember Me
           </label>
         </div>
-        <div class="form-group">
+        <div className="form-group">
         <button disabled={formState.isSubmitting} className="btn btn-primary">
                             {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
                             Login
